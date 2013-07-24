6c9f772655bbd97b1a7a7de56d24861bdc7a13b7
Marcus Baw <marcusbaw@gmail.com>
2013-07-24 09:36:57 -0400

    added models_sketch

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sakebu::Application.config.secret_token = '692e796de446c469a03b244cfc0a3251e34073a8257e6923fefcd3777d5f0626e5909a1d9fa702599e1e7e6c67df73e540641baf49bb89ce6ad3362285fa4c12'
