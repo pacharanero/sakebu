03694f9f50316ace2d9f911be3538af326a28b4b
Marcus Baw <marcusbaw@gmail.com>
2013-08-02 13:08:03 -0400

    basic view.html.erb

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   # attr_accessible :title, :body
+  
+  has_many : dictations
+  
 end
